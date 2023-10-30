07367a5c03352c81f743f7f1ec2ef7849b476bd9
GuerreroM05 <daimerguerrerofuratena@gmail.com>
2023-10-29 20:10:16 -0500

    documentacion de codigo

--- app.js
+++ app.js
@@ -5,28 +5,38 @@ require("colors");
 const math = require("./modules/mate.js")
 
 /*console.log(math);*/
-
+// Definición de la función principal como una función asíncrona
 const  main = async() => {
-    console.clear();
-    let a= (math.getRandomInt(10));
-    let b=(math.getRandomInt(10));
+    console.clear(); // Limpia la consola
+
+    // Genera dos números aleatorios utilizando la función getRandomInt del objeto math
+    let a= (math.getRandomInt(10)); // Genera el primer número aleatorio
+    let b=(math.getRandomInt(10)); // Genera el segundo número aleatorio
+
+    // Imprime una serie de líneas de asteriscos en azul
     console.log("***************************************".blue);
     console.log("*     ".blue,"funciones matematicas".bgBlue,
     "     *".blue);
     console.log("***************************************\n".blue);
     console.log("***************************************".red);
-    console.log("         numeros aleatorios: "+" a "+"b" );
+
+    // Imprime los valores de los números aleatorios generados
+    console.log("         numeros aleatorios: "+ a + " " + b );
     console.log("*******************************************\n".blue);
 
-
+    // Imprime una serie de líneas de asteriscos en azul
     console.log("*******************************************".blue);
+
+    // Imprime líneas que muestran las operaciones matemáticas con los números aleatorios generados
     console.log("//".blue +"                                       "+" //".blue);
-    console.log("//".blue +"        add:" + math.add(a,b) + "           "+"              //".blue);
-    console.log("//".blue +"        substract:" + math.substract(a,b) +"      "+"            //".blue);
-    console.log("//".blue +"        multiply:" + math.multiply (a,b) +"        "+"             //".blue);
-    console.log("//".blue +"        divide:" + math.divide (a,b) + "            "+"     //".blue);       
+    console.log("//".blue +"        add:" + math.add(a,b) + "           "+"              //".blue); // Imprime la suma de los números aleatorios
+    console.log("//".blue +"        substract:" + math.substract(a,b) +"      "+"            //".blue); // Imprime la resta de los números aleatorios
+    console.log("//".blue +"        multiply:" + math.multiply (a,b) +"        "+"             //".blue); // Imprime el producto de los números aleatorios
+    console.log("//".blue +"        divide:" + math.divide (a,b) + "            "+"     //".blue);       // Imprime el cociente de los números aleatorios, maneja la división entre cero
+
+    // Imprime una serie de líneas de asteriscos en azul
     console.log("*******************************************\n".blue);
-   
 }
 
+// Ejecuta la función principal
 main();
--- modules/mate.js
+++ modules/mate.js
@@ -1,34 +1,35 @@
-const math = {};
+const math = {}; // Se inicializa un objeto vacío llamado 'math'
 
-function add(a,b) {
-    return a + b;
+function add(a,b) { // Se define una función de suma que toma dos parámetros 'a' y 'b'
+    return a + b; // Devuelve la suma de 'a' y 'b'
 }
 
-function substract(a,b) {
-    return a - b;
+function substract(a,b) { // Se define una función de resta que toma dos parámetros 'a' y 'b'
+    return a - b; // Devuelve la resta de 'a' y 'b'
 }
 
-function multiply(a,b){
-    return a * b;
+function multiply(a,b){ // Se define una función de multiplicación que toma dos parámetros 'a' y 'b'
+    return a * b; // Devuelve el producto de 'a' y 'b'
 }
 
-function divide(a,b){
-    if (b == 0){
-        return "Error no se puede dividir entre cero";
-    }else {
-        return a / b;
-    }
-    
+function divide(a,b){ // Se define una función de división que toma dos parámetros 'a' y 'b'
+    if (b == 0){ // Comprueba si 'b' es igual a cero
+        return "Error no se puede dividir entre cero"; // Devuelve un mensaje de error si 'b' es cero
+    } else {
+        return a / b; // Devuelve el cociente de 'a' dividido por 'b' si 'b' no es cero
+    }  
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function getRandomInt(max) { // Se define una función para generar un número entero aleatorio
+    return Math.floor(Math.random() * max); // Devuelve un número aleatorio entre 0 y 'max - 1'
 }
 
+// Asigna las funciones definidas anteriormente como propiedades del objeto 'math'
 math.add = add;
 math.substract = substract;
 math.multiply = multiply;
 math.divide = divide;
 math.getRandomInt = getRandomInt;
 
+// Exporta el objeto 'math' para su uso en otros archivos de JavaScript mediante la declaración 'require'
 module.exports = math;
